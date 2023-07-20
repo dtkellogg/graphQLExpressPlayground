02ad06bcb2097faa5f45aadfa61d17e9376634fe
Toshi <dtkellogg10@gmail.com>
2023-07-20 10:37:09 -0400

    GraphQL - set up graphical interface at localhost:5000

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql')
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Hello world',
+    fields: () => ({
+      message: { 
+        type: GraphQLString,
+        resolve: () => 'Hello World'
+      }
+    })
+  })
+})
+
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}))
 app.listen(5000, () => console.log("Server Running"))
